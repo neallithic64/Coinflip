a53210cce16a990aab852357ab1400867b778b5a
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2019-12-01 21:34:13 +0800

    Attempted to get LCCoCF
    LCCoCF: longest consecutive chain of coin faces, for the lack of a shorter term

--- coinflip.c
+++ coinflip.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
 	if (argc == 2) {
-		int coinCtr = /*100*/atoi(argv[1]);
+		int coinCtr = atoi(argv[1]);
 		time_t t; srand((unsigned) time(&t));
 		int Hctr = 0, Tctr = 0, next = rand()%2, last, longest = 0;
 		
@@ -23,9 +23,8 @@ int main(int argc, char **argv) {
 		}
 		
 		printf("\n\tCoin Flip Statistics:\n");
-		printf("Heads: %d, Percentage: %.2lf%%\nTails: %d, Percentage: %.2lf%%\n",
-			Hctr, (double) Hctr/coinCtr*100, Tctr, (double) Tctr/coinCtr*100);
-		printf("Longest chain: %d\n", longest);
+		printf("Heads: %d, Percentage: %.2lf%%\nTails: %d, Percentage: %.2lf%%\n", Hctr, (double) Hctr/coinCtr*100, Tctr, (double) Tctr/coinCtr*100);
+		printf("Longest chain (buggy): %d\n", longest);
 	} else {
 		printf("wrong parameter count\n");
 	}
